708ba2c198cbea1de89f1037fae77d8a4ab850eb
flooprea <62398104+flooprea@users.noreply.github.com>
2020-04-12 14:10:54 +0100

    project-complete
    Project completed.

--- readme.txt
+++ readme.txt
@@ -1,3 +1,8 @@
-Challanges:
+Challenges:
     1. Refactor the code so that the errors disspaear
     2. "name" variable is scoped to tightly. Refactor the code to avoid duplication.
+
+Additional stuff done:
+    1. Created HTML index file to test the output in the browser on the page and not just on the console.
+    2. Created the input box and made the name input possible
+    3. Added the 'Reload' button 
--- trainig-days-project.js
+++ trainig-days-project.js
@@ -1,5 +1,5 @@
-// The scope of `random` is too loose 
 const random = Math.floor(Math.random() * 3);
+let name = prompt("Please enter your name");
 
 const getRandEvent = () => {
   if (random === 0) {
@@ -13,33 +13,37 @@ const getRandEvent = () => {
 
 // The scope of `days` is too tight 
 const getTrainingDays = event => {
-    
+    let days = 0;
   if (event === 'Marathon') {
-    let days = 50;
+     days = 50;
   } else if (event === 'Triathlon') {
-    let days = 100;
+     days = 100;
   } else if (event === 'Pentathlon') {
-    let days = 200;
+     days = 200;
   }
 
   return days;
 };
 
 // The scope of `name` is too tight 
-const logEvent = event => {
-  const name = 'Nala';
-  console.log(`${name}'s event is: ${event}`);
+const logEvent = (name, event) => {
+    return `${name}'s event is: ${event}`
 };
 
-const logTime = days => {
-  const name = 'Nala';
-  console.log(`${name}'s time to train is: ${days} days`);
+const logTime = (name, days) => {
+  return `${name}'s time to train is: ${days} days`
 };
 
 const event = getRandEvent();
 const days = getTrainingDays(event);
-// Define a `name` variable. Use it as an argument after updating logEvent and logTime 
+
+//display output
+document.getElementById('output').innerHTML = logEvent(name, event) + ' | ' + logTime(name, days);
+
+//reload button
+document.getElementById("button").addEventListener("click", function(){
+    location.reload();
+});
 
 
-logEvent(event);
-logTime(days);
+
